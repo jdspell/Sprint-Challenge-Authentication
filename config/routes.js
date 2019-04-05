2de5e13e5e98c248f7430bd2477fe82f0b004716
jdspell <spelljd15@gmail.com>
2019-04-05 12:15:14 -0400

    implemented register funciton that hashes password

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,7 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+
+const db = require('../database/dbConfig.js');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -8,12 +11,42 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
-function register(req, res) {
+async function register(req, res) {
   // implement user registration
+  try{
+    const newUser = req.body;
+
+    newUser.password = bcrypt.hashSync(newUser.password, 8);
+
+    const [id] = await db('users').insert(newUser);
+
+    const user = await db('users')
+      .where({ id: id })
+      .first();
+  
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 }
 
-function login(req, res) {
+async function login(req, res) {
   // implement user login
+  try {
+    const { username, password } = req.body;
+
+    // const user = await db('users')
+    //   .where({ username: username })
+    //   .first();
+    
+    // if(user && bcrypt.compareSync(user.password, password)){ 
+    //   res.status(200).json({ message: "logged in" });
+    // } else {
+    // }
+
+  } catch (error) {
+    res.status(500).json(error);
+  }
 }
 
 function getJokes(req, res) {
